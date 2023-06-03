6ee33e4ef5925a740c89b6b77da8cb393f24b9e8
Tom Forbes <tom@tomforb.es>
2023-06-04 00:49:20 +0100

    Support multiple DSNs

--- diffable_sql/cli.py
+++ diffable_sql/cli.py
@@ -5,16 +5,19 @@ from sqlalchemy.schema import CreateTable, CreateIndex
 
 
 @click.command()
-@click.argument("dsn")
-def cli(dsn):
-    engine = sqlalchemy.create_engine(dsn)
-    metadata_obj = MetaData()
-    metadata_obj.reflect(bind=engine)
+@click.argument("dsn_list", metavar="[dsn]...", nargs=-1)
+def cli(dsn_list):
     output_ddl = []
+    tables = []
+    for dsn in dsn_list:
+        engine = sqlalchemy.create_engine(dsn)
+        metadata_obj = MetaData()
+        metadata_obj.reflect(bind=engine)
+        tables.extend((engine, table) for table in metadata_obj.tables.values())
 
-    tables = sorted(metadata_obj.tables.values(), key=lambda t: t.name)
+    sorted_tables = sorted(tables, key=lambda t: t[1].name)
 
-    for table in tables:
+    for (engine, table) in sorted_tables:
         create_table_ddl = CreateTable(table, include_foreign_key_constraints=table.foreign_key_constraints)
         create_table_ddl.columns = sorted(create_table_ddl.columns, key=lambda c: c.element.name)
         output_ddl.append(str(create_table_ddl.compile(engine)).strip() + '\n')
